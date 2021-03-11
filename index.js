a7abc9b397894ab7620a8caca17a15065b4c2712
TornadoMC <hunterjohanson04@gmail.com>
2021-03-11 23:52:23 +0000

    stuff

--- index.js
+++ index.js
@@ -11,18 +11,6 @@ function clean(text) {
       return text;
 }
 
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-
 let queue = {};
 let nowPlaying = {};
 
