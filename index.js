7ce4fde2ccb34c45576d691ff59e3e773cdf37c0
TornadoMC <hunterjohanson04@gmail.com>
2021-03-12 19:07:36 +0000

    yes

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ const commands = {
 
     } catch {
 
-      await search(url).then(async (res) => {
+      await search(url, async (res) => {
 
         if (!queue.hasOwnProperty(message.guild.id)) queue[message.guild.id] = {}, queue[message.guild.id].playing = false, queue[message.guild.id].songs = [];
       
